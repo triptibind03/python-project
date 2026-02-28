e7d83db310895b49527d92ec024118429d2c3e64
Tripti <triptibind03@gmail.com>
2026-02-28 13:53:21 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,7 @@
+#simple calculator
+a= int(input("enter first digit"))
+b= int(input("enter 2nd digit"))
+print("addition: ", a+b)
+print("subtraction: ", a-b)
+print("division", a/b)
+print("multiplication", a*b)
