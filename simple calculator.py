8461527832f2c0fd8a53e7ea3b679a41ff7199a6
Tripti <triptibind03@gmail.com>
2026-02-28 14:05:14 +0530

    0

--- simple calculator.py
+++
@@ -1,7 +0,0 @@
-#simple calculator
-a= int(input("enter first digit"))
-b= int(input("enter 2nd digit"))
-print("addition: ", a+b)
-print("subtraction: ", a-b)
-print("division", a/b)
-print("multiplication", a*b)
